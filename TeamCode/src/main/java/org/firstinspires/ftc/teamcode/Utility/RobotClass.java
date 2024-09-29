c4094fd128faa21b702f074b5e3e45a7d93e7448
Matt B <110642574+BeitM@users.noreply.github.com>
2024-09-29 13:40:11 -0400

    Teleop and Robot class change

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utility/RobotClass.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utility/RobotClass.java
@@ -4,9 +4,11 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.Range;
 import org.firstinspires.ftc.robotcontroller.external.samples.RobotHardware;
+
 public class RobotClass{
     private LinearOpMode myOpMode = null;
 
@@ -29,6 +31,9 @@ public class RobotClass{
     public static final double ARM_UP_POWER    =  0.45 ;
     public static final double ARM_DOWN_POWER  = -0.45 ;
 
+    //Drive Robot Variables
+    public static double speedVar = 1;
+
     // Define a constructor that allows the OpMode to pass a reference to itself.
     public RobotClass (LinearOpMode opmode) {
         myOpMode = opmode;
@@ -38,22 +43,41 @@ public class RobotClass{
     public void init()    {
 
         //Wheel Motors
-        frontLeftDrive  = myOpMode.hardwareMap.get(DcMotor.class, "frontLeftDrive");
-        frontRightDrive = myOpMode.hardwareMap.get(DcMotor.class, "frontRightDrive");
-        backLeftDrive   = myOpMode.hardwareMap.get(DcMotor.class, "backLeftDrive");
-        backRightDrive  = myOpMode.hardwareMap.get(DcMotor.class, "backRightDrive");
+        //frontLeftDrive  = myOpMode.hardwareMap.get(DcMotor.class, "frontLeftDrive");
+        //frontRightDrive = myOpMode.hardwareMap.get(DcMotor.class, "frontRightDrive");
+        //backLeftDrive   = myOpMode.hardwareMap.get(DcMotor.class, "backLeftDrive");
+        //backRightDrive  = myOpMode.hardwareMap.get(DcMotor.class, "backRightDrive");
+
+        frontLeftDrive  = myOpMode.hardwareMap.get(DcMotor.class, "leftfront_drive");
+        frontRightDrive = myOpMode.hardwareMap.get(DcMotor.class, "rightfront_drive");
+        backLeftDrive   = myOpMode.hardwareMap.get(DcMotor.class, "leftback_drive");
+        backRightDrive  = myOpMode.hardwareMap.get(DcMotor.class, "rightback_drive");
+
 
         //Wheel Directions
         frontLeftDrive.setDirection(DcMotor.Direction.REVERSE);
         frontRightDrive.setDirection(DcMotor.Direction.FORWARD);
+        backLeftDrive.setDirection(DcMotorSimple.Direction.REVERSE);
+
+
 
         //Other Motors
-        armMotor = myOpMode.hardwareMap.get(DcMotor.class, "arm");
+        //armMotor = myOpMode.hardwareMap.get(DcMotor.class, "arm");
 
         //Servos
-        leftHand = myOpMode.hardwareMap.get(Servo.class, "left_hand");
-        rightHand = myOpMode.hardwareMap.get(Servo.class, "right_hand");
-        leftHand.setPosition(MID_SERVO);
-        rightHand.setPosition(MID_SERVO);
+        //leftHand = myOpMode.hardwareMap.get(Servo.class, "left_hand");
+        //rightHand = myOpMode.hardwareMap.get(Servo.class, "right_hand");
+        //leftHand.setPosition(MID_SERVO);
+        //rightHand.setPosition(MID_SERVO);
     }
+
+    public void driveRobot(double fl, double fr, double bl, double br)
+    {
+        // Send calculated power to wheels
+        frontLeftDrive.setPower(fl);
+        frontRightDrive.setPower(fr);
+        backLeftDrive.setPower(bl);
+        backRightDrive.setPower(br);
+    }
+
 }
